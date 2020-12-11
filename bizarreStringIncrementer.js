9bdb18292f4dfa04384f0938b77d585c4be71909
bhk31 <bhkhandelwal92@gmail.com>
2020-12-11 23:53:29 +0530

    bizarreStringIncrementer

--- bizarreStringIncrementer.js
+++ bizarreStringIncrementer.js
@@ -1,1 +1,48 @@
-// Start your implementation here
+function bizarreStringIncrementer() {
+let lastCharacter = str.charCodeAt(str.length - 1);
+
+  if (lastCharacter >= 65 && lastCharacter <= 90 ||
+    lastCharacter >= 97 && lastCharacter <= 122) {
+    console.log(str + 1);
+  }
+
+  if (lastCharacter >= 48 && lastCharacter <= 56) {
+    console.log(str.slice(0, -1) + (parseInt(str.charAt(str.length - 1)) + 1));
+  }
+
+
+  if (lastCharacter === 57) {
+    let revArray = str.slice(0, -1).split("").reverse();
+    let arrNum = []
+    let hasZero = false;
+
+    for (let i = 0; i < revArray.length; i++) {
+      let char = revArray[i];
+      if (char.charCodeAt(0) >= 49 && char.charCodeAt(0) <= 57) {
+        arrNum.push(e);
+      } else if (char.charCodeAt(0) == 48) {
+        hasZero = true;
+        break;
+      }
+    }
+
+    let increNum = (parseInt(arrNum.join('') + str.charAt(str.length - 1)) + 1)
+    let numLen = increNum.toString(10).length
+    if (hasZero) {
+      var end = numLen
+    } else {
+      var end = numLen - 1
+    }
+    console.log(str.slice(0, end * (-1)) + increNum)
+  }
+}
+
+bizarreStringIncrementer('foo');
+bizarreStringIncrementer('foo23');
+bizarreStringIncrementer('foo0041');
+bizarreStringIncrementer('foo9');
+bizarreStringIncrementer('foo099');
+bizarreStringIncrementer('f99oo');
+bizarreStringIncrementer('f99oo23');
+bizarreStringIncrementer('f99oo099');
+bizarreStringIncrementer('f99oo0099');
